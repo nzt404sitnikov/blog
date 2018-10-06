508be4e57b1e0652dcf608d3b72368e7c4e476cc
nzt404sitnikov <nzt404sitnikov@yandex.ru>
2018-10-07 02:28:43 +0300

    My start template

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=blog',
+    'username' => 'postgres',
+    'password' => 'evezin17',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
